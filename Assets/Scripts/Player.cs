bc6702f21c31e2e9115416898b145948cac7b6a8
Sekwah <sekwah41@gmail.com>
2018-08-11 16:44:30 +0100

    Added player movement code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,10 @@ namespace Game
 
         CharacterController controller;
 
+        Vector3 direction = new Vector3(0,0,0);
+
+        public float playerSpeed = 3;
+
         // Use this for initialization
         void Start()
         {
@@ -29,7 +33,17 @@ namespace Game
 
             Vector3 velocity = new Vector3(x, 0, z);
 
+            if(x*x + z * z < 1)
+            {
+                direction = velocity.normalized * playerSpeed;
+            }
+            else
+            {
+                direction = velocity * playerSpeed;
+            }
 
+            controller.Move(direction * Time.deltaTime);
+            this.transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
         }
     }
 }
