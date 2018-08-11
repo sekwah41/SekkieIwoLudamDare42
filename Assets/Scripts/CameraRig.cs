a3f4349106f12ebc0497c1912f84aa5a622a83d6
GoblinBob <gobbobminecraft@gmail.com>
2018-08-11 14:43:33 +0200

    Made great files.

---
+++ Assets/Scripts/CameraRig.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class CameraRig : MonoBehaviour
+    {
+
+        Player player;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
