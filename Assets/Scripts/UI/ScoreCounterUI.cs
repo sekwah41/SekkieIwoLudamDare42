522a4a7bc27a769dfe0427d4ef482b35e1a62203
Sekwah <sekwah41@gmail.com>
2018-08-12 14:59:59 +0100

    Score number now slides to new value

--- Assets/Scripts/UI/ScoreCounterUI.cs
+++ Assets/Scripts/UI/ScoreCounterUI.cs
@@ -7,6 +7,12 @@ namespace Game.UI
 
         TextMeshProUGUI textComponent;
 
+        int score = 0;
+
+        int displayedScore = 0;
+
+        float displayedScoref = 0;
+
         // Use this for initialization
         void Start() {
             GameManager.Instance.Points.RegisterOnChangedHandler(HandleScoreChanged);
@@ -15,14 +21,34 @@ namespace Game.UI
             UpdateText(GameManager.Instance.Points.Value);
         }
 
+        private void Update()
+        {
+            if(displayedScore != score)
+            {
+                float movePercent = 4f * Time.deltaTime;
+                if (movePercent > 1)
+                {
+                    movePercent = 1;
+                }
+                displayedScoref += (score - displayedScoref) * movePercent;
+                int updateScore = (int) Mathf.Round(displayedScoref);
+                if (updateScore != displayedScore)
+                {
+                    UpdateText(updateScore);
+                }
+                
+            }
+        }
+
         void UpdateText(int score)
         {
+            displayedScore = score;
             textComponent.text = "" + score;
         }
 
         void HandleScoreChanged(int newScore)
         {
-            UpdateText(newScore);
+            score = newScore;
         }
     }
 }
