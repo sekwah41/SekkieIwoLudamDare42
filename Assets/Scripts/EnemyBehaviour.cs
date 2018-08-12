a9e623e1755ece6c8ec2f45317cdf5e62fb50b1c
Sekwah <sekwah41@gmail.com>
2018-08-12 20:01:34 +0100

    Added AI stuff

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -7,16 +7,37 @@ namespace Game
     {
         public ColorType color;
 
+        Player target;
+
+        private NavMeshPath path;
+        private float recalculate = 0.0f;
+
         NavMeshAgent agent;
 
         void Start()
         {
             agent = GetComponent<NavMeshAgent>();
+            target = GameManager.Instance.player;
+            path = new NavMeshPath();
+            recalculate = 0.0f;
         }
 
         void Update()
         {
-            agent.SetDestination(new Vector3(0, 0, 100));
+            //agent.SetDestination(target.transform.position);
+
+            recalculate += Time.deltaTime;
+            if (recalculate > 0.5f)
+            {
+                recalculate -= 0.5f;
+                NavMesh.CalculatePath(transform.position, target.transform.position, NavMesh.AllAreas, path);
+                if(path.status != NavMeshPathStatus.PathInvalid)
+                {
+                    agent.SetPath(path);
+                }
+            }
+            for (int i = 0; i < path.corners.Length - 1; i++)
+                Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
         }
 
     }
