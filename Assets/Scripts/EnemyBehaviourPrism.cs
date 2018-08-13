6ff3f60658122b6342da9b53c40ed59e116ec0bf
Sekwah <sekwah41@gmail.com>
2018-08-13 21:59:27 +0100

    Made prism code

---
+++ Assets/Scripts/EnemyBehaviourPrism.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Game
+{
+    public class EnemyBehaviourPrism : EnemyBehaviour
+    {
+
+        float angleDiff = Mathf.PI * 2 / 15F;
+
+        override public bool HitByBullet(Bullet bullet)
+        {
+            Debug.Log("EXPLOSION");
+            float yOrientation = bullet.transform.rotation.y;
+            for(int i = -1; i < 2; i++)
+            {
+                Shoot(new Vector3(Mathf.Sin(yOrientation * Mathf.PI / 180.0F + i * angleDiff), 0, Mathf.Cos(yOrientation * Mathf.PI / 180.0F + i * angleDiff)));
+            }
+            Destroy(gameObject);
+            return true;
+        }
+
+        private void Shoot(Vector3 direction)
+        {
+            GameObject bulletObject = Instantiate(GameManager.Instance.bulletPrefab);
+            bulletObject.transform.SetPositionAndRotation(transform.position, Quaternion.identity);
+            Bullet bullet = bulletObject.GetComponent<Bullet>();
+            bullet.SetColor(ColorType);
+            bullet.SetVelocity(direction.normalized * GameManager.Instance.bulletSpeed);
+        }
+    }
+}
